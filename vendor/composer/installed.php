867c4f63bf7540faa90f7153310cd9f29029ac84
Kemo <ksonko@sfu.ca>
2022-01-11 17:48:46 -0800

    added composer dependeciees

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1b87bbee2ba610f98c96250721538245e66ad936',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1b87bbee2ba610f98c96250721538245e66ad936',
+            'dev_requirement' => false,
+        ),
+    ),
+);
